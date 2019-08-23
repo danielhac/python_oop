f23c65227e3576ce72403965a9d1f9d13f34f764
Dan <danhboi23@hotmail.com>
2019-08-22 23:24:20 -0700

    Created class and static methods

--- practice.py
+++ practice.py
@@ -1,5 +1,7 @@
 class Jobs:
 
+    jobseeker = "Daniel"
+
     def __init__(self, company, url, title):
         self.company = company
         self.url = url
@@ -7,6 +9,18 @@ class Jobs:
     
     def print_func(self):
         print("Company name: " + self.company + "\t\t Title: " + self.title + "\t\t URL: " + self.url)
+    
+    @classmethod
+    def get_job_seeker(cls):
+        print(cls.jobseeker)
+    
+    @staticmethod
+    def get_info():
+        print("This static method will not have instance nor class variables")
+
 
 job1 = Jobs("Kaiser", "Intern", "kp.org")
 job1.print_func()
+job1.get_job_seeker()
+
+Jobs.get_info()
