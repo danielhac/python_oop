fafb7fc0c90dd01e42452f997900b02dd1360635
Dan <danhboi23@hotmail.com>
2019-08-24 14:57:32 -0700

    added example of method resolution order (leftmost will be called)

--- constr_inheritance.py
+++ constr_inheritance.py
@@ -2,6 +2,9 @@
 #   Subclass will call superclass' constructor if subclass has no constructor.
 #   Otherwise, subclass will use its own constructor.
 
+# Method Resolution Order (MRO): if 2+ superclass' constructor or same named methods exist
+# when calling super() from subclass, it will choose the leftmost one.
+
 # Superclass WITHOUT constructor
 class A:
     def feature_A(self):
@@ -15,6 +18,15 @@ class B:
 
     def feature_B(self):
         print("feature_B1")
+
+
+# Superclass WITH constructor
+class BB:
+    def __init__(self):
+        print("constructor BB")
+
+    def feature_BB(self):
+        print("feature_BB1")
 
 
 # Subclass of A WITH constructor
@@ -49,8 +61,17 @@ class F(B):
         print("feature_F1")
 
 
+# Subclass of A and B WITH constructor
+# Method Resolution Order (MRO), will call BB's constructor since it is leftmost
+class G(BB,B):
+    def __init__(self):
+        super().__init__()
+        print("constructor G")
+
+
 # Create and run one at a time to see what is happening  
 # c1 = C()            # no  constructor in A, yes constructor in C
 # d1 = D()            # yes constructor in B, no  constructor in D
 # e1 = E()            # yes constructor in B, yes constructor in D
-f1 = F()            # yes constructor in B, yes constructor in D
+# f1 = F()            # yes constructor in B, yes constructor in D
+g1 = G()            # yes constructor in BB, yes constructor in B
