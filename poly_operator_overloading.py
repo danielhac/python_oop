ecf3ea5258a30967ee5789b29e410238b7d34a49
Dan <danhboi23@hotmail.com>
2019-08-24 19:03:15 -0700

    init: operator overloading

---
+++ poly_operator_overloading.py
@@ -0,0 +1,20 @@
+# Demonstrating polymorphism
+#   Operator overloading: giving extended meaning beyond their predefined operational meaning.
+#   Ex: add two integers as well as join two strings and merge two lists
+
+# This example takes two separate objects' passed in numbers 1, 5 and uses
+# operator overloading of the __add__ method so the class can use the '+' symbol.
+class Plus:
+    def __init__(self, a):
+        self.a = a
+    
+    # Operator overloading so that the operator can use the '+' symbol
+    def __add__(self, second):
+        return self.a + second.a
+
+
+s1 = Plus(1)
+s2 = Plus(5)
+
+s3 = s1 + s2
+print(s3)
